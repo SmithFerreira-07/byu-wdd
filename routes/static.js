09e6c4f01df7b7957e5e27274e5b71496e3da8aa
SmithFerreira-07 <GitFerreira@proton.me>
2025-07-05 12:03:38 -0300

    layout, fixing css

--- routes/static.js
+++ routes/static.js
@@ -2,16 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // Static Routes
-// Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
-// Index Route
-router.get("/", function(req, res) {
-    res.render("index", { title: "Home" });
-});
 
 module.exports = router;
 
