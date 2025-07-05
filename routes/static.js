a7305f7c3c5ec3d1224990f2744cd89e6bc3634e
SmithFerreira-07 <GitFerreira@proton.me>
2025-07-05 10:21:21 -0300

    second

--- routes/static.js
+++ routes/static.js
@@ -8,11 +8,11 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
-module.exports = router;
-
-//Index Route
-app.get("/", function(req, res) {
+// Index Route
+router.get("/", function(req, res) {
     res.render("index", { title: "Home" });
 });
+
+module.exports = router;
 
 
