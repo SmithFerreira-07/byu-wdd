da38b1d903785acd495c2d5217a359e3fcc0f638
SmithFerreira-07 <GitFerreira@proton.me>
2025-07-26 17:47:39 -0300

    final fixes

--- utilities/inventory-validation.js
+++ utilities/inventory-validation.js
@@ -11,7 +11,9 @@ validate.classificationRules = () => {
       body("classification_name")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Please provide a classification name."), // on error this message is sent.
+        .withMessage("Please provide a classification name.")
+        .matches(/^[a-zA-Z0-9]+$/)
+        .withMessage("Classification name cannot contain spaces or special characters."),
     ]
   }
 
@@ -24,61 +26,61 @@ validate.inventoryRules = () => {
       body("classification_id")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Please provide a classification."), // on error this message is sent.
+        .withMessage("Please provide a classification."),
 
       // inv_make is required and must be string
       body("inv_make")
         .trim()
         .isLength({ min: 3 })
-        .withMessage("Please provide a make."), // on error this message is sent.
+        .withMessage("Please provide a make."),
 
       // inv_model is required and must be string
       body("inv_model")
         .trim()
         .isLength({ min: 3 })
-        .withMessage("Please provide a model."), // on error this message is sent.
+        .withMessage("Please provide a model."),
 
       // inv_year is required and must be a 4-digit number
       body("inv_year")
         .trim()
         .isLength({ min: 4, max: 4 })
-        .withMessage("Please provide a 4-digit year."), // on error this message is sent.
+        .withMessage("Please provide a 4-digit year."),
 
       // inv_description is required and must be string
       body("inv_description")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Please provide a description."), // on error this message is sent.
+        .withMessage("Please provide a description."),
 
       // inv_image is required and must be string
       body("inv_image")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Please provide an image path."), // on error this message is sent.
+        .withMessage("Please provide an image path."),
 
       // inv_thumbnail is required and must be string
       body("inv_thumbnail")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Please provide a thumbnail path."), // on error this message is sent.
+        .withMessage("Please provide a thumbnail path."),
 
       // inv_price is required and must be a number
       body("inv_price")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Please provide a price."), // on error this message is sent.
+        .withMessage("Please provide a price."),
 
       // inv_miles is required and must be a number
       body("inv_miles")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Please provide miles."), // on error this message is sent.
+        .withMessage("Please provide miles."),
 
       // inv_color is required and must be string
       body("inv_color")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Please provide a color."), // on error this message is sent.
+        .withMessage("Please provide a color."),
     ]
     }
 
