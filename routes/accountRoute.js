57b66f36e96a7374d234fa3081b05572efe5530d
SmithFerreira-07 <GitFerreira@proton.me>
2025-07-26 17:13:12 -0300

    validation client and server

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 // GET route for My Account page
 router.get("/", utilities.handleErrors(accountController.buildAccount));
@@ -15,7 +16,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // GET route for Registration page
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-// POST route for Registration form submission
-router.post('/register', utilities.handleErrors(accountController.registerAccount));
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
